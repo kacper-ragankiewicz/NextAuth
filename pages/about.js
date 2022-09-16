ec175c6a31305c44a3cfe3a879f4039ef4f8ede5
kacper-ragankiewicz <kacper-ragankiewicz@wp.pl>
2022-09-16 20:18:11 +0200

    Testing

---
+++ pages/about.js
@@ -0,0 +1,5 @@
+export default function AboutMe() {
+    return (
+        <h1>Its me Mario!</h1>
+    )
+}
---
+++ pages/memes/[id].js
@@ -0,0 +1,34 @@
+
+
+export default function Mem({ mem: {name, url} }) {
+    console.log("Jestem na Froncie")
+    return (
+        <>
+            <h1>mem</h1>
+            <img src={url} alt={`Mem ${name}`}/>
+        </>
+    )
+}
+
+export async function getServerSideProps(context) {
+    const { id } = context.params;
+    const response = await fetch('https://api.imgflip.com/get_memes');
+    const { data, success } = await response.json();
+
+    if (!success) {
+        return {
+            redirect: {
+                destination: '/',
+                pernament: false,
+            }
+        }
+    }
+
+    const mem = data.memes.find(mem => mem.id === id);
+
+    return {
+        props: {
+            mem,
+        }
+    }
+}
