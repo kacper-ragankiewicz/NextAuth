258cd7d14e728f032fbf3925fed01c8e3ae1ad53
kacper-ragankiewicz <kacper-ragankiewicz@wp.pl>
2022-09-23 20:57:04 +0200

    Changes on forms

--- pages/login/index.js
+++ pages/login/index.js
@@ -2,14 +2,25 @@ import styles from './Login.module.scss';
 
 export default function login() {
     return(
-        <div className={styles.form}>
-            <form action="/api" method="post">
-                    <label for='login'>Login:</label>
-                    <input type="text" id="login" name="login"/>
-                    <label for='pass'>Password:</label>
-                    <input type="text" id="pass" name="pass" />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
+            <div className={styles.form}>
+                <form action="/api/form" method="post">
+                        <label htmlFor='login'>Login:</label>
+                        <input
+                            type="text"
+                            id="login"
+                            name="login"
+                        />
+                        <label htmlFor='pass'>Password:</label>
+                        <input
+                            type="text"
+                            id="pass"
+                            name="pass"
+                            required
+                            minLength="8"
+                            maxLength="20"
+                        />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
     )
 }
